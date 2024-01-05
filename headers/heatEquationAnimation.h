7b929d51d50052d9beb2db05e9cfef994b081641
Ania Polidori <ania.polidori@ensiie.fr>
2024-01-05 21:10:57 +0100

    Commentaires'

--- headers/heatEquationAnimation.h
+++ headers/heatEquationAnimation.h
@@ -6,10 +6,30 @@
 #include "heatEquationBarre.h"
 
 
+/**
+ * @brief Fonction qui initialise une fenêtre SDL
+ * 
+ * @return SDL_Window* 
+ */
 SDL_Window *initWindow();
 
+
+/**
+ * @brief Fonction qui initialise un rendu pour une fenêtre SDL
+ * 
+ * @param window : fenêtre SDL
+ * @return SDL_Renderer* 
+ */
 SDL_Renderer *initRenderer(SDL_Window *window);
 
+
+/**
+ * @brief Fonction qui joue l'animation de l'équation de la chaleur sur un certain temps
+ * 
+ * @param renderer : rendu SDL où l'animation est joué
+ * @param u0 : température initiale
+ * @param solution : matrice qui contient toutes les solutions de l'équation de la chaleur 
+ */
 void animationBarre(SDL_Renderer *renderer, double u0, const std::vector<std::vector<double>> &solution);
 
 
